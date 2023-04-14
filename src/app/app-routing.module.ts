457a5bfc7926cd2711ff4a50fe375ce30a37a1c2
Paul Alonte <paul.alonte@labforward.io>
2023-04-14 22:29:03 +0200

    update dependencies

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,17 +1,21 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-import { ProductlistComponent } from './productlist/productlist.component';
-import { AddProductComponent } from './addproduct/addproduct.component';
-import { ProductDetailComponent } from './productdetail/productdetail.component';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { ProductlistComponent } from "./productlist/productlist.component";
+import { AddProductComponent } from "./addproduct/addproduct.component";
+import { ProductDetailComponent } from "./productdetail/productdetail.component";
 
 const routes: Routes = [
-  { path: 'addproduct', component: AddProductComponent},
-  { path: 'productlist', component: ProductlistComponent},
-  { path: 'productdetail/:id', component: ProductDetailComponent, data:{product: 'product'}}
+  { path: "addproduct", component: AddProductComponent },
+  { path: "productlist", component: ProductlistComponent },
+  {
+    path: "productdetail/:id",
+    component: ProductDetailComponent,
+    data: { product: "product" },
+  },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
